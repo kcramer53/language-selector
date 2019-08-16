a83719f007f0d9dd554e854e01704b6198a7577e
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-08-16 13:33:56 -0700

    Adds images, styles and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    var age = parseFloat($("input#age").val());
+   console.log(age);
+   var desire = $("input:checkbox[name=desire]:checked").val();
+   console.log(desire);
+   if (age >= 18 && desire==="Y") {
+     $("#pass-age").show();
+   } else if (age<18) {$(".screen").show("Sorry you need to be 18 to adopt a furry friend. Come back when you are older!");
+     }
+     else if (desire==="N"){$("#no-desire").show();
+     } else {
+       alert('Please enter your age.');
+   }
+ });
+
+
+
+    $("#button").click(function(){
+          var learn = $("input:radio[name=learn]:checked").val();
+          if (learn==="real") {
