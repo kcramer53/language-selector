b0096a99d4611b659d3ecad2c20e40a8b5e0dbf5
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-08-16 16:25:55 -0700

    reworks code attempts erros corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,32 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
-    var langage = $("#language").val();
-    var learn = $("input:radio[name=learn]:checked").val();
-    var food = $("input:radio[name=food]:checked").val();
-    var fitness = $("input:radio[name=fitness]:checked").val();
-    var love = $("input:radio[name=love]:checked").val();
-    var music = $("input:radio[name=music]:checked").val();
+    var learn = parseInt($("input:radio[name=learn]:checked").val());
+    var food = parseInt($("input:radio[name=food]:checked").val());
+    var fitness = parseInt($("input:radio[name=fitness]:checked").val());
+    var love = parseInt($("input:radio[name=love]:checked").val());
+    var music = parseint($("input:radio[name=music]:checked").val());
 
    }
  });
 
 
-
- $("#button").click(function(){
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    $("#button").click(function(){
    var lang = $("input:radio[name=learn]:checked").val();
    if (learn==="real" && food==="really" && fitness==="realist" && love==="realer" && music==="realing") {
-     $("#img1").hide();
-     $("#img2").hide();
-     $("#img3").hide();
-     $("#img4").show();
+     $('#img1",#img2,#img3').hide();
+     $('#img4').show
    }
   else if (learn==="achiever" && food==="acievers" && fitness==="achieving" && love==="achievinger" && music==="acivingers") {
-     $("#img1").hide();
-     $("#imj2").hide();
-     $("#img4").hide();
-     $("#img3").show();
+    $('#img1",#img2,#img4').hide();
+    $('#img3').show
    }
   else if (learn==="unknnown" && food==="unknowns" && fitness==="unknowing" && love==="unknowingly" && music==="knowingly") {
-      $("#img1").hide();
-      $("#imj3").hide();
-      $("#img4").hide();
-      $("#img2").show();
+    $('#img1",#img4,#img3').hide();
+    $('#img2').show
    }
    else (learn==="real" && food==="unknowns" && fitness==="achieving" && love==="really" && music==="knowingly") {
-     $("#img3").hide();
-     $("#imj2").hide();
-     $("#img4").hide();
-     $("#img1").show();
+     $('#img4",#img2,#img3').hide();
+     $('#img1').show
    }
    event.preventDefault();
  });
